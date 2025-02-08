e2bb328bdd1fcf0df9fead67d230917ff0b9c8da
aexli2 <159849911+aexli2@users.noreply.github.com>
2025-02-07 19:18:32 -0600

    Add files via upload

---
+++ Introduction.js
@@ -0,0 +1,10 @@
+
+var BoxVisibility="hidden";
+
+let myVariable = "New Text from JavaScript";
+
+// Get the element
+let heading = document.getElementById("TEXTT");
+
+// Update the text
+document.getElementById("TEXTT").innerHTML = myVariable;
